bd474bd2288405de790a3f683036a678e85b3ab8
Maurizio D'Addona <mauritiusdadd@gmail.com>
2023-11-24 22:22:28 +0100

    Add loader function

--- gui.py
+++ gui.py
@@ -5,16 +5,71 @@ Created on Fri Nov 24 10:33:40 2023.
 
 @author: daddona
 """
+import os
 import sys
 import typing
-from PySide6 import QtCore, QtGui, QtUiTools, QtWidgets
+import logging
+
+
+from utils import loadSpectrum
+import backends
+
+try:
+    from PySide6 import QtCore, QtGui, QtUiTools, QtWidgets
+except (ImportError, ModuleNotFoundError):
+    from PyQt6 import QtCore, QtGui, QtWidgets, uic
+    QT_BACKEND = 'PyQt6'
+else:
+    QT_BACKEND = 'PySide6'
 
 
 class GuiApp():
     """General class for the main GUI."""
 
-    def __init__(self):
-        self.main_wnd = loadUiWidget("main_window.ui")
+    def __init__(self, qt_backend: str):
+        self.qapp = QtWidgets.QApplication.instance()
+        if self.qapp is None:
+            # if it does not exist then a QApplication is created
+            self.qapp = QtWidgets.QApplication(sys.argv)
+
+        self.open_spectra = {}
+
+        self.main_wnd = loadUiWidget("main_window.ui", qt_backend=qt_backend)
+
+        self.main_wnd.importSpecPushButton.clicked.connect(
+            self.doImportSpectra
+        )
+
+
+    def doImportSpectra(self, *args, **kwargs):
+        """
+        Use QFileDialog to get spectra files.
+
+        Parameters
+        ----------
+        *args : TYPE
+            DESCRIPTION.
+        **kwargs : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        file_list, files_type = QtWidgets.QFileDialog.getOpenFileNames(
+            self.main_wnd,
+            self.qapp.tr("Import Spectra"),
+            '.',
+            (
+                f"{self.qapp.tr('FITS')} (*.fit *.fits);;"
+                f"{self.qapp.tr('ASCII')} (*.txt *.dat *.cat);;"
+                f"{self.qapp.tr('All Files')} (*.*)"
+            )
+        )
+
+        for file in file_list:
+            sp = loadSpectrum(file)
 
     def run(self):
         """
@@ -25,23 +80,20 @@ class GuiApp():
         None.
 
         """
-        app = QtWidgets.QApplication.instance()
-        if app is None:
-            # if it does not exist then a QApplication is created
-            app = QtWidgets.QApplication(sys.argv)
         self.main_wnd.show()
-        sys.exit(app.exec())
+        sys.exit(self.qapp.exec())
 
 
 class GuiAppPyside(GuiApp):
     """Main App GUI in PySide."""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(QT_BACKEND)
 
 
 def loadUiWidget(uifilename: str,
-                 parent: typing.Optional[QtWidgets.QWidget] = None
+                 parent: typing.Optional[QtWidgets.QWidget] = None,
+                 qt_backend: typing.Optional[str] = 'PySide6'
                  ) -> QtWidgets.QWidget:
     """
     Load an UI file.
@@ -59,11 +111,21 @@ def loadUiWidget(uifilename: str,
         The widget loaded from the UI file.
 
     """
-    loader = QtUiTools.QUiLoader()
-    uifile = QtCore.QFile(uifilename)
-    uifile.open(QtCore.QFile.ReadOnly)
-    ui = loader.load(uifile, parent)
-    uifile.close()
+    logging.debug(f"Loading UI using Qt backend '{qt_backend}'")
+    ui_file_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), 'ui',
+        uifilename
+    )
+
+    if qt_backend == 'PySide6':
+        loader = QtUiTools.QUiLoader()
+        uifile = QtCore.QFile(ui_file_path)
+        uifile.open(QtCore.QFile.ReadOnly)
+        ui = loader.load(uifile, parent)
+        uifile.close()
+    elif qt_backend == 'PyQt6':
+        ui = uic.loadUi(ui_file_path)
+
     return ui
 
 
---
+++ utils.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 20:38:29 2023.
+
+@author: daddona
+"""
+import os
+from typing import Union, Optional
+
+import numpy as np
+
+from astropy.nddata import VarianceUncertainty
+from astropy.io import fits
+from astropy import wcs
+from astropy import units
+from specutils import Spectrum1D
+from specutils.io.registers import data_loader
+
+KNOWN_SPEC_EXT_NAMES = ['spec', 'spectrum', 'flux', 'data', 'sci', 'science']
+KNOWN_VARIANCE_EXT_NAMES = ['stat', 'stats', 'var', 'variance', 'noise', 'err']
+KNOWN_INVAR_EXT_NAMES = ['ivar', 'ivariance']
+KNOWN_MASK_EXT_NAMES = ['mask', 'platemask', 'footprint', 'dq', 'nan_mask']
+KNOWN_WAVE_EXT_NAMES = ['wave', 'wavelenght', 'lambda', 'lam']
+KNOWN_RCURVE_EXT_NAMES = ['r', 'reso', 'resolution', 'rcurve', 'wd']
+KNOWN_RGB_EXT_NAMES = ['r', 'g', 'b', 'red', 'green', 'blue']
+
+
+def identifySpecexFits(origin, *args, **kwargs):
+    """
+    Identify spectra extracted with python-specex.
+
+    Parameters
+    ----------
+    origin : TYPE
+        DESCRIPTION.
+    *args : TYPE
+        DESCRIPTION.
+    **kwargs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    """
+    if not (
+        isinstance(args[0], str) and
+        os.path.splitext(args[0].lower())[1] == '.fits'
+    ):
+        return False
+
+    valid_id_keys = [
+        f"{i}{j}"
+        for i in ['', 'OBJ', 'OBJ_', 'TARGET', 'TARGET_']
+        for j in ['ID', 'NUMBER', 'UID', 'UUID']
+    ]
+
+    with fits.open(args[0]) as hdul:
+        for hdu in hdul:
+            for key in valid_id_keys:
+                try:
+                    _ = hdu.header[key]
+                except KeyError:
+                    continue
+                else:
+                    return True
+    return False
+
+
+@data_loader("specex-1d", identifier=identifySpecexFits, extensions=['fits'])
+def specexFitsLoader(file_name: str,
+                     flux_hdu_index: Optional[Union[str, int]] = None,
+                     resolution: Union[int, float] = 1,
+                     **kwargs):
+    """
+    Load a python-specex spectrum.
+
+    Parameters
+    ----------
+    file_name : TYPE
+        DESCRIPTION.
+    **kwargs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    def getHDU(hdul, valid_names, index=None):
+        if index is None:
+            for hdu in hdul:
+                if hdu.name.lower() in valid_names:
+                    return hdu
+            else:
+                return None
+        else:
+            return hdul[index]
+
+    with fits.open(file_name, **kwargs) as hdulist:
+        flux_hdu = getHDU(hdulist, KNOWN_SPEC_EXT_NAMES)
+        var_hdu = getHDU(hdulist, KNOWN_VARIANCE_EXT_NAMES)
+        mask_hdu = getHDU(hdulist, KNOWN_MASK_EXT_NAMES)
+        wd_hdu = getHDU(hdulist, KNOWN_RCURVE_EXT_NAMES)
+
+
+        if flux_hdu is None:
+            raise ValueError("Cannot find flux data")
+        flux = flux_hdu.data
+
+        if var_hdu is None:
+            var = np.ones_like(flux)
+        else:
+            var = var_hdu.data
+
+        if mask_hdu is None:
+            mask = None
+        else:
+            mask = mask_hdu.data
+
+        if wd_hdu is None:
+            wd = None
+        else:
+            wd = wd_hdu.data
+
+        flux_wcs = wcs.WCS(flux_hdu.header)
+
+        if flux.shape != var.shape:
+            raise ValueError(
+                "Spectral data invalid or corruptede: Flux data shape "
+                "do not match variance data one!"
+            )
+
+        # NOTE: Wavelenghts must be in Angstrom units
+        pixel = np.arange(len(flux))
+        if flux_wcs.has_spectral:
+            lam = flux_wcs.pixel_to_world(pixel).Angstrom
+        else:
+            try:
+                coeff0 = flux_hdu.header["COEFF0"]
+                coeff1 = flux_hdu.header["COEFF1"]
+            except KeyError:
+                raise ValueError("Catton determine wavelength mapping")
+            lam = 10**(coeff0 + coeff1*pixel)
+        flux = flux.astype('float32')
+
+        flux_not_nan_mask = ~np.isnan(flux)
+
+        if mask is not None:
+            flux_not_nan_mask &= ~mask
+
+        flux_units = units.Unit(flux_hdu.header['BUNIT'])
+        wave_units = units.Unit(flux_hdu.header['CUNIT1'])
+
+        print(flux_units, wave_units)
+
+        flux = flux[flux_not_nan_mask].copy() * flux_units
+        var = var[flux_not_nan_mask].copy() * (flux_units**2)
+        lam = lam[flux_not_nan_mask].copy() * wave_units
+
+        print(flux)
+        print(var)
+
+        if wd is not None:
+            wd = wd[flux_not_nan_mask].copy()
+        else:
+            # If now wavelenght dispersion information is present, then
+            # compute it using the wavelenght
+            delta_lambda = np.ones_like(lam)
+            delta_lambda[1:] = (lam[1:] - lam[:-1])
+            delta_lambda = delta_lambda.to('Angstrom').value
+            wd = resolution / delta_lambda
+            wd[0] = wd[1]
+        wd[wd < 1e-3] = 2.
+
+        meta = {'header': hdulist[0].header}
+        uncertainty = VarianceUncertainty(var)
+
+        sp = Spectrum1D(
+            flux=flux,
+            spectral_axis=lam,
+            wcs=flux_wcs,
+            uncertainty=uncertainty,
+            meta=meta
+        )
+
+        sp.mask = mask
+        sp.wd = wd
+
+    return sp
+
+
+def loadSpectrum(url: str):
+    """
+    Load a spectrum.
+
+    Parameters
+    ----------
+    url : str
+        THe url of the file containing a spectrum.
+
+    Raises
+    ------
+    NotImplementedError
+        DESCRIPTION.
+
+    Returns
+    -------
+    sp : specutils.Spectrum1D
+        The spectrum.
+
+    """
+    try:
+        sp = Spectrum1D.read(url)
+    except Exception:
+        raise NotImplementedError("No loader available")
+    else:
+        return sp
